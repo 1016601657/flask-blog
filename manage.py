8796a6a198b5353d98a408d948adf5b514fdd4ff
1016601657 <13120321516@163.com>
2015-06-26 15:55:35 +0800

    test-request

--- manage.py
+++ manage.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,5 +9,12 @@ def index():
 @app.route('/hello/<name>')
 def hello(name):
 	return 'hello %s'%name
+
+@app.route('/re')
+def re():
+	result = request.headers.get('user-agent')
+	return result
+
 if __name__ == '__main__':
+	app.debug=True
 	app.run()
