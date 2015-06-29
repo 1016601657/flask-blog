3a08bbb08645d818dc1e7e6d3e8c3c2ddd3ddd29
1016601657 <13120321516@163.com>
2015-06-29 18:17:58 +0800

    test

--- mysql.py
+++ mysql.py
@@ -1,16 +1,26 @@
-from flask import Flask
-from flask.ext.script import Shell
-from flask.ext.script import Manager
+from flask import Flask, render_template, session, redirect, url_for, flash
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.script import Shell,Manager
+import MySQLdb
 
 app = Flask(__name__)
 manager = Manager(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/test'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
 @app.route('/')
 def hello():
     return 'gaga'
 
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Role=Role)
+    return dict(app=app)
+
 manager.add_command("shell", Shell(make_context=make_shell_context))
 
 if __name__ == '__main__':
